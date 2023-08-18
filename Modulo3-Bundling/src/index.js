9be3f53c2f86635cbaf2e753737242a8d40622c9
RBAMOUSER\canom7 <Nilymar1982>
2023-08-18 14:22:12 +0200

    Adding logo

--- Modulo3-Bundling/src/index.js
+++ Modulo3-Bundling/src/index.js
@@ -1,4 +1,6 @@
 import "./styles.scss";
-const userName = "mire";
+import logoImg from "./content/logo_1.png";
 
-const hello = `Hello ${userName}`;
+const img = document.createElement("img");
+img.src = logoImg;
+document.getElementById("imgContainer").appendChild(img);
